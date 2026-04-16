519ae29945e5ec569b92b186c766e2f236590231
Ashwath Nadahalli <anadahalli@gmail.com>
2026-04-16 23:24:15 +0530

    client: make create_async_client method async

--- src/pyclickhouse/client.py
+++ src/pyclickhouse/client.py
@@ -35,10 +35,14 @@ class Client(AiohttpAsyncClient):
         )
 
 
-def create_async_client(**kwargs: Any) -> Client:
+async def create_async_client(**kwargs: Any) -> Client:
     """
     Create a new ClickHouse client.
 
     Refer to `clickhouse_connect.create_async_client` for more details.
     """
-    return Client(**kwargs)
+    client = Client(**kwargs)
+
+    await client._initialize()
+
+    return client
